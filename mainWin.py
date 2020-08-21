a3cee9b9f673d806571d665c5abdab5cde65ee18
jackzhang <zhangch06@pcl.ac.cn>
2020-08-21 15:56:01 +0800

    set ICO and Windows title

--- mainWin.py
+++ mainWin.py
@@ -1,11 +1,17 @@
 import sys
 from mainWinUI import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+
+class  configPage(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(configPage, self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle('FMC205基带射频配置')
+        self.setWindowIcon(QIcon('rss1.ico'))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mainWin = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(mainWin)
+    mainWin = configPage()
     mainWin.show()
     sys.exit(app.exec_())
