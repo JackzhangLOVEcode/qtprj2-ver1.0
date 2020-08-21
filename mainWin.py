08a8bd945b78f2b7fdce8d5fb555c1a4d69e62ff
jackzhang <zhangch06@pcl.ac.cn>
2020-08-21 15:38:33 +0800

    show UI without slot function

--- mainWin.py
+++ mainWin.py
@@ -1,1 +1,11 @@
-print('hello')
+import sys
+from mainWinUI import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWin = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(mainWin)
+    mainWin.show()
+    sys.exit(app.exec_())
